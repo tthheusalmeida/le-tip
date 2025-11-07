26d46e2679da8cf6c1881cd076f9366c0d01dfda
Matheus Almeida <dev.matheus.almeida@gmail.com>
2025-11-07 09:46:15 -0300

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HeaderComponent from './components/molecules/HeaderComponent.vue'
+</script>
+
+<template>
+  <div class="app">
+    <HeaderComponent />
+
+    <main></main>
+  </div>
+</template>
+
+<style scoped lang="css">
+.app {
+  padding: 1rem;
+}
+</style>
---
+++ src/components/molecules/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header header--dark">
+    <h3 class="header__title">Lip Tip</h3>
+    <div class="header__theme">Tema</div>
+  </header>
+</template>
+
+<style scoped lang="css">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &__title {
+    font-weight: 600;
+    font-size: 1.25rem;
+  }
+
+  &__theme {
+    opacity: 0.8;
+  }
+}
+</style>
